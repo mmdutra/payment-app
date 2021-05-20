923a893746edef9a75fcde4dba20a4da29fc42c7
Mateus Morais <mmdutra14@gmail.com>
2021-05-19 22:12:06 -0300

    bind guzzle client interface with implementation

--- app/Base/Providers/AppServiceProvider.php
+++ app/Base/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Base\Providers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,5 +11,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(\Faker\Generator::class, fn () => \Faker\Factory::create('pt_BR'));
+        $this->app->bind(ClientInterface::class, Client::class);
     }
 }
